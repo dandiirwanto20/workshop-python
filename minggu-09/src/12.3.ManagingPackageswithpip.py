47184f73b69b71a1382393f8220f556326107002
colab-CSD-035 <dandi.ir20@gmail.com>
2022-04-24 05:26:20 +0700

    Pertemuan minggu 9 Bab 12 dan Conda

---
+++ minggu-09/src/12.3.ManagingPackageswithpip.py
@@ -0,0 +1,10 @@
+# Anda dapat menginstal versi terbaru dari suatu paket dengan menyebutkan nama suatu paket:
+# (tutorial-env) $ python -m pip install novas
+# Output
+"""
+Collecting novas
+  Downloading novas-3.1.1.3.tar.gz (136kB)
+Installing collected packages: novas
+  Running setup.py install for novas
+Successfully installed novas-3.1.1.3
+"""
